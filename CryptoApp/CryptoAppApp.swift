1f52062e6c06565a4c17bc5ff340ba93d87324ee
cansuozdizlekli <101900120+cansuozdizlekli@users.noreply.github.com>
2024-04-29 18:25:19 +0300

    initial commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Cansu Özdizlekli on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
