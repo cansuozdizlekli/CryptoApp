a527425177c2343a695d5ee2dfa931b541ab12fc
cansuozdizlekli <101900120+cansuozdizlekli@users.noreply.github.com>
2024-04-30 10:20:09 +0300

    added color theme to project

---
+++ CryptoApp/Extension/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Cansu Özdizlekli on 4/30/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
